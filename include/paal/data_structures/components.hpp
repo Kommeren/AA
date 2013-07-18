8bed6181c101049a1c409086f8835c536805c3be
Piotr Wygocki <wygos@mimuw.edu.pl>
2013-07-18 08:47:48 +0200

    SwapCOmponent added
    Change-Id: Id5718a7328bdcaedb0f11607c2f6dd0a1f132f2c

--- include/paal/data_structures/components.hpp
+++ include/paal/data_structures/components.hpp
@@ -13,6 +13,7 @@
 #include <boost/mpl/zip_view.hpp>
 #include <boost/mpl/fold.hpp>
 #include <boost/mpl/map.hpp>
+#include <boost/mpl/find.hpp>
 #include <boost/fusion/container/map.hpp>
 #include <boost/fusion/include/map.hpp>
 #include <boost/fusion/view/zip_view.hpp>
@@ -28,86 +29,173 @@ namespace data_structures {
 
 struct NoDefault;
 
-template <typename... ComponentNames>
+
+namespace detail {
+    template <typename Names, typename Types>
     class Components {
-    static const int N = sizeof...(ComponentNames);
+        static const int N = boost::mpl::size<Names>::type::value;
+        static_assert(boost::mpl::size<Types>::type::value == N, "Incrrect number of parameters");
+        typedef typename boost::mpl::zip_view<boost::mpl::vector<Names, Types>> NamesWithTypes;
+        
+        template <template <class, class> class Pair>
+        struct NamesWithTypesToPairs {
+        typedef typename boost::mpl::transform_view <
+                                NamesWithTypes,
+                                boost::mpl::unpack_args<Pair<boost::mpl::_1, 
+                                                             boost::mpl::_2>>
+                             >::type type;
+        };
+
+        typedef typename boost::mpl::fold<
+                typename NamesWithTypesToPairs<boost::mpl::pair>::type,
+                boost::mpl::map<>,
+                boost::mpl::insert<boost::mpl::_1, boost::mpl::_2>
+            >::type NameTypeMap;
+
+        template <typename Name>
+        struct TypeForName {
+            typedef typename boost::mpl::at<NameTypeMap, Name>::type type;
+        };
+
+        //TODO we do not support object which are not default constructible
+        template <int k, typename Component, typename... ComponentTypesPrefix>
+        void setComonentsPartial(Component component, ComponentTypesPrefix... components) {
+            typedef typename boost::mpl::at<Names, boost::mpl::int_<k>>::type Name;
+            boost::fusion::at_key<Name>(m_components) = component;
+            setComonentsPartial<k+1>(components...);
+        }
+       
+        //boundary condition
+        template <int k>
+        void setComonentsPartial() {}
+
     public:
-        template <typename... ComponentTypes>
-        class type {
-            static_assert(sizeof...(ComponentTypes) == N, "Incrrect number of parameters");
-            typedef typename boost::mpl::vector<ComponentNames...> Names;
-            typedef typename boost::mpl::vector<ComponentTypes...> Types;
-            typedef typename boost::mpl::zip_view<boost::mpl::vector<Names, Types>> NamesWithTypes;
-            
-            template <template <class, class> class Pair>
-            struct NamesWithTypesToPairs {
-            typedef typename boost::mpl::transform_view <
-                                    NamesWithTypes,
-                                    boost::mpl::unpack_args<Pair<boost::mpl::_1, 
-                                                                 boost::mpl::_2>>
-                                 >::type type;
-            };
+        typedef typename boost::fusion::result_of::as_map<
+                typename NamesWithTypesToPairs<boost::fusion::pair>::type
+            >::type ComponentsMap;
+        
+        Components() = default;
+        
+        Components(Components &&) = default;
 
-            typedef typename boost::fusion::result_of::as_map<
-                    typename NamesWithTypesToPairs<boost::fusion::pair>::type
-                >::type Components;
-            
-            typedef typename boost::mpl::fold<
-                    typename NamesWithTypesToPairs<boost::mpl::pair>::type,
-                    boost::mpl::map<>,
-                    boost::mpl::insert<boost::mpl::_1, boost::mpl::_2>
-                >::type ComponentsMap;
+        Components(const Components &) = default;
+        
+        Components& operator=(const Components &) = default;
 
-            template <typename Name>
-            struct TypeForName {
-                typedef typename boost::mpl::at<ComponentsMap, Name>::type type;
-            };
+        Components& operator=(Components &&) = default;
 
-            //TODO we do not support object which are not default constructible
-            template <int k, typename Component, typename... ComponentTypesPrefix>
-            void setComonentsPartial(Component component, ComponentTypesPrefix... components) {
-                typedef typename boost::mpl::at<Names, boost::mpl::int_<k>>::type Name;
-                boost::fusion::at_key<Name>(m_components) = component;
-                setComonentsPartial<k+1>(components...);
-            }
-            
-            template <int k>
-            void setComonentsPartial() {}
+        template <typename... ComponentTypesPrefix>
+        Components(ComponentTypesPrefix... components) {
+           setComonentsPartial<0>(components...); 
+        }
+        
+        template <typename ComponentName, typename... Args>
+        auto call(Args&&... args) ->
+        decltype(std::declval<typename TypeForName<ComponentName>::type>()(std::forward<Args>(args)...)) {
+            return boost::fusion::at_key<ComponentName>(m_components)(std::forward<Args>(args)...);
+        }
 
-        public:
-            type() = default;
-            
-            type(ComponentTypes... componets) : m_components(componets...) {}
-            
+        template <typename ComponentName>
+        void set(const typename TypeForName<ComponentName>::type & comp) {
+            boost::fusion::at_key<ComponentName>(m_components) = comp;
+        }
+        
+        template <typename ComponentName>
+        typename boost::fusion::result_of::at_key<ComponentsMap, ComponentName>::type & get() {
+            return boost::fusion::at_key<ComponentName>(m_components);
+        }
+        
+        template <typename ComponentName>
+        const typename boost::fusion::result_of::at_key<const ComponentsMap, ComponentName>::type & get() const {
+            return boost::fusion::at_key<ComponentName>(m_components);
+        }
 
-            template <typename... ComponentTypesPrefix>
-            type(ComponentTypesPrefix... components) {
-               setComonentsPartial<0>(components...); 
-            }
-            
-            
-            template <typename ComponentName, typename... Args>
-            auto call(Args&&... args) ->
-            decltype(std::declval<typename TypeForName<ComponentName>::type>()(std::forward<Args>(args)...)) {
-                return boost::fusion::at_key<ComponentName>(m_components)(std::forward<Args>(args)...);
-            }
+    private:
 
-            template <typename ComponentName>
-            void set(const typename TypeForName<ComponentName>::type & comp) {
-                boost::fusion::at_key<ComponentName>(m_components) = comp;
-            }
-            
-            template <typename ComponentName>
-            typename boost::fusion::result_of::at_key<Components, ComponentName>::type & get() {
-                return boost::fusion::at_key<ComponentName>(m_components);
-            }
-            
+        ComponentsMap m_components;
+    };
+};
 
-        private:
 
-            Components m_components;
+//implementation on variadic templates is imposible because of
+//week support for type detection for inner template classes
+template <typename... ComponentNames>
+class Components {
+    typedef typename boost::mpl::vector<ComponentNames...> Names;
+public:
+    template <typename... ComponentTypes>
+    using type = typename detail::Components<Names, typename boost::mpl::vector<ComponentTypes...>>;
+};
+
+
+template <typename Name, typename NewType, typename Components> class SwapType;
+
+template <typename Name, typename NewType, typename Names, typename Types> 
+class SwapType<Name, NewType, detail::Components<Names, Types>> {
+public:
+    typedef typename boost::mpl::find<Names, Name>::type::pos pos; // position to insert
+    typedef typename boost::mpl::begin<Types>::type TypesBegin; // begin iterator
+    typedef typename boost::mpl::advance<TypesBegin, pos>::type TypeIter; // iterator on position
+    typedef typename boost::mpl::erase<Types, TypeIter>::type TypesErased; // removed old element
+    typedef typename boost::mpl::begin<TypesErased>::type ErasedBegin; // begin of new collection with erased element
+    typedef typename boost::mpl::advance<ErasedBegin, pos>::type TypeIterErased; // correct place in collection with erased element
+    typedef typename boost::mpl::insert<TypesErased, TypeIterErased, NewType>::type TypesSwapped; // insert new element  (final collection) 
+    typedef detail::Components<Names, TypesSwapped> type;
+};
+
+
+namespace detail {
+    template <typename T>
+    struct WrapToConstructable {
+        typedef T type;
+    };
+
+    template <typename OldComps, typename NewComps>
+    class Rewrite {
+    public:
+        Rewrite(const OldComps & old, NewComps & nw) : 
+            m_old(old), m_new(nw) {}
+
+        template <typename NameWrap>
+        void operator()(NameWrap a) {
+            typedef typename NameWrap::type Name;
+            m_new.template set<Name>(std::move(m_old.template get<Name>()));
+        }
+
+    private:
+        const OldComps & m_old;
+        NewComps & m_new;
+    };
+    
+    template <typename OldComps, typename NewComps>
+    Rewrite<OldComps, NewComps>
+    make_Rewrite(OldComps & old, NewComps & nw) {
+        return Rewrite<OldComps, NewComps>(old, nw);
+    }
+
+    struct ToConstructable {
+        template <typename T>
+        struct apply {
+            typedef WrapToConstructable<T> type;
         };
     };
+}
+
+template <typename Name, typename NewType, typename Names, typename Types> 
+typename SwapType<Name, NewType, detail::Components<Names, Types> >::type
+swap(NewType comp, detail::Components<Names, Types> components){
+    typename SwapType<Name, NewType, detail::Components<Names, Types> >::type resComponents;
+    
+    typedef typename boost::mpl::find<Names, Name>::type pos; 
+    typedef typename boost::mpl::erase<Names, pos>::type NamesErased; // removed old element
+
+    boost::mpl::for_each(detail::make_Rewrite(components, resComponents), 
+                         static_cast<NamesErased*>(0), 
+                         static_cast<detail::ToConstructable*>(0));
+    resComponents.template set<Name>(comp);
+
+    return std::move(resComponents);
+}
 
 } //data_structures
 }//paal
--- test/data_structures/components_test.cpp
+++ test/data_structures/components_test.cpp
@@ -8,12 +8,18 @@
 #define BOOST_TEST_MODULE components
 
 #include <boost/test/unit_test.hpp>
+
+#include <type_traits>
+#include <boost/mpl/equal.hpp>
+
 #include "paal/data_structures/components.hpp"
 
-namespace comps {
-    struct CompA;
-    struct CompB;
-    struct CompC;
+
+
+namespace names {
+    struct A;
+    struct B;
+    struct C;
 }
 
 int f(int i) {
@@ -22,7 +28,7 @@ int f(int i) {
 
 template <typename... Args>
 using Comps = typename  paal::data_structures::Components<
-        comps::CompA, comps::CompB, comps::CompC>::type<Args...> ;
+        names::A, names::B, names::C>::type<Args...> ;
 
         
 namespace bm = boost::mpl;
@@ -32,20 +38,37 @@ BOOST_AUTO_TEST_CASE(ComponentsTest) {
 
     //default
     Comps<int, double, int> comps;
-    comps.get<comps::CompA>();
-    comps.set<comps::CompA>(7);
-    BOOST_CHECK_EQUAL(comps.get<comps::CompA>(), 7);
+    comps.get<names::A>();
+    comps.set<names::A>(7);
+    BOOST_CHECK_EQUAL(comps.get<names::A>(), 7);
     
     Comps<int, double, int> comps2(5,4,3);
-    BOOST_CHECK_EQUAL(comps2.get<comps::CompA>(), 5);
+    BOOST_CHECK_EQUAL(comps2.get<names::A>(), 5);
 
     //default value for 3rd argument
     Comps<int, double, int> comps3(5,4);
-    BOOST_CHECK_EQUAL(comps3.get<comps::CompA>(), 5);
+    BOOST_CHECK_EQUAL(comps3.get<names::A>(), 5);
     
+    typedef Comps<int(*)(int), double, int> CompsF;
     
-    Comps<int(*)(int), double, int> comps4;
+    CompsF comps4;
 
-    Comps<int(*)(int), double, int> comps5(f);
-    BOOST_CHECK_EQUAL(comps5.call<comps::CompA>(2), 2);
+    CompsF comps5(f, 2, 17);
+
+    BOOST_CHECK_EQUAL(comps5.call<names::A>(2), 2);
+
+    typedef paal::data_structures::SwapType<names::A, std::pair<int, int>, CompsF>::type Swapped;
+
+    //This is unfortunately not true
+    //typedef Comps<std::pair<int, int>, double, int> SwappedCheck;
+//    static_assert(std::is_same<Swapped, SwappedCheck>::value, "Invalid swapped type");
+
+    Swapped swap = paal::data_structures::swap<names::A>(std::make_pair(11, 12), comps5);
+    
+    auto p = swap.get<names::A>();
+    BOOST_CHECK_EQUAL(p.first, 11);
+    BOOST_CHECK_EQUAL(p.second, 12);
+    BOOST_CHECK_EQUAL(swap.get<names::B>(), 2);
+    BOOST_CHECK_EQUAL(swap.get<names::C>(), 17);
+
 }
