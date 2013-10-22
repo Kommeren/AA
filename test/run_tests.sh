86c0aeaafd6aa6a4f5eed9f1b081c61f9f00657e
Piotr Wygocki <wygocki@siekiera.mimuw.edu.pl>
2013-10-22 08:31:02 +0200

    tests refinements
    Change-Id: I01465ff201cb982cc59ce47f514caeb686de162b

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,7 +1,7 @@
 if [[ $# != 1 ]]; then
-echo help:
-echo "$0 <build_dir>"
-exit
+    echo help:
+    echo "$0 <build_dir>"
+    exit
 fi
 
 exit_on_error() {
@@ -11,97 +11,26 @@ exit_on_error() {
     fi
 }
 
+tests="2_local_search_test bounded_degree_mst_test capacitated_voronoi_test cycle_canceling_test dreyfus_wagner_test facility_location_test
+       facility_location_neghborhood_getter_test facility_location_solution_adapter_test facility_location_solution_test generalised_assignment_test
+       k_median_test k_median_solution_adapter_test local_search_multi_solution_test local_search_multi_solution_lambdas_test local_search_test
+       local_search_obj_fun_test metric_test successive_shortest_path_test simple_cycle_test splay_tree_test splay_cycle_test steiner_network_test
+       zelikovsky_11_per_6_test subset_iterator_test vertex_to_edge_iterator_test voronoi_test tree_augmentation_test 
+       scheduling_jobs_with_deadlines_on_a_single_machine_test knapsack_test scheduling_jobs_on_identical_parallel_machines_test k_cut_test"
+
+option() {
+    ret=""
+    case $1 in
+        *) 
+            ;;
+    esac
+    $ret
+}
+
 build_dir=$1
-echo ------------------------------------------- 2_local_search_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/2_local_search_test
-exit_on_error
-echo ------------------------------------------- bounded_degree_mst_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/bounded_degree_mst_test
-exit_on_error
-echo ------------------------------------------- capacitated_voronoi_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/capacitated_voronoi_test
-exit_on_error
-echo ------------------------------------------- cycle_canceling_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/cycle_canceling_test
-exit_on_error
-echo ------------------------------------------- dreyfus_wagner_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/dreyfus_wagner_test
-exit_on_error
-echo ------------------------------------------- facility_location_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/facility_location_test
-exit_on_error
-echo ------------------------------------------- facility_location_neghborhood_getter_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/facility_location_neghborhood_getter_test
-exit_on_error
-echo ------------------------------------------- facility_location_solution_adapter_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/facility_location_solution_adapter_test
-exit_on_error
-echo ------------------------------------------- facility_location_solution_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/facility_location_solution_test
-exit_on_error
-echo ------------------------------------------- generalised_assignment_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/generalised_assignment_test
-exit_on_error
-echo ------------------------------------------- k_median_solution_adapter_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/k_median_solution_adapter_test
-exit_on_error
-echo ------------------------------------------- k_median_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/k_median_test
-exit_on_error
-echo ------------------------------------------- local_search_multi_solution_lambdas_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/local_search_multi_solution_lambdas_test
-exit_on_error
-echo ------------------------------------------- local_search_multi_solution_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/local_search_multi_solution_test
-exit_on_error
-echo ------------------------------------------- local_search_obj_fun_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/local_search_obj_fun_test
-exit_on_error
-echo ------------------------------------------- local_search_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/local_search_test
-exit_on_error
-echo ------------------------------------------- metric_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/metric_test
-exit_on_error
-echo ------------------------------------------- successive_shortest_path_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/successive_shortest_path_test
-exit_on_error
-echo ------------------------------------------- simple_cycle_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/simple_cycle_test
-exit_on_error
-echo ------------------------------------------- splay_cycle_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/splay_cycle_test
-exit_on_error
-echo ------------------------------------------- splay_tree_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/splay_tree_test
-exit_on_error
-echo ------------------------------------------- steiner_network_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/steiner_network_test
-exit_on_error
-echo ------------------------------------------- zelikovsky_11_per_6_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/zelikovsky_11_per_6_test
-exit_on_error
-echo ------------------------------------------- subset_iterator_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/subset_iterator_test
-exit_on_error
-echo ------------------------------------------- vertex_to_edge_iterator_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/vertex_to_edge_iterator_test
-exit_on_error
-echo ------------------------------------------- voronoi_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/voronoi_test
-exit_on_error
-echo ------------------------------------------- tree_augmentation_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/tree_augmentation_test
-exit_on_error
-echo ------------------------------------------- scheduling_jobs_on_identical_parallel_machines_test      -------------------------------------------------------------------------------------
-./${build_dir}/test/scheduling_jobs_on_identical_parallel_machines_test
-exit_on_error
-echo ------------------------------------------- knapsack_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/knapsack_test
-exit_on_error
-echo ------------------------------------------- scheduling_jobs_with_deadlines_on_a_single_machine_test      -------------------------------------------------------------------------------------
-./${build_dir}/test/scheduling_jobs_with_deadlines_on_a_single_machine_test
-exit_on_error
-echo ------------------------------------------- k_cut_test      -------------------------------------------------------------------------------------
-./${build_dir}/test/k_cut_test
-exit_on_error
+
+for t in $tests; do
+    echo ------------------------------------------- $t 	 -------------------------------------------------------------------------------------
+    ./${build_dir}/test/$t $(option $t)
+    exit_on_error
+done
--- test/run_tests_long.sh
+++ test/run_tests_long.sh
@@ -1,7 +1,7 @@
 if [[ $# != 1 ]]; then
-echo help:
-echo "$0 <build_dir>"
-exit
+    echo help:
+    echo "$0 <build_dir>"
+    exit
 fi
 
 exit_on_error() {
@@ -11,50 +11,31 @@ exit_on_error() {
     fi
 }
 
+option() {
+    ret=""
+    case $1 in
+        "2_local_search_long_test")
+            ret="--run_test=TSPLIB"
+            ;;
+        "capacitated_facility_location_long_test")
+            ret="--run_test=FacilityLocationLong"
+            ;;
+        *) 
+            ;;
+    esac
+    echo $ret
+}
+
+tests="shortest_superstring_long_test 2_local_search_long_test bimap_perf_test bounded_degree_mst_long_test tree_augmentation_long_test 
+       capacitated_facility_location_long_test dreyfus_wagner_long_test facility_location_long_test generalised_assignment_long_test
+       steiner_network_long_test zelikovsky_11_per_6_long_test k_median_long_test scheduling_jobs_on_identical_parallel_machines_long_test
+       knapsack_two_app_long_test knapsack_long_test"
+
 build_dir=$1
-echo ------------------------------------------- shortest_superstring_long_test          -------------------------------------------------------------------------------------
-./${build_dir}/test/shortest_superstring_long_test
-exit_on_error
-echo ------------------------------------------- 2_local_search_long_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/2_local_search_long_test --run_test=TSPLIB
-exit_on_error
-echo ------------------------------------------- bimap_perf_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/bimap_perf_test
-exit_on_error
-#echo ------------------------------------------- bounded_degree_mst_long_test     -------------------------------------------------------------------------------------
-#./${build_dir}/bounded_degree_mst_long_test
-#exit_on_error
-echo ------------------------------------------- tree_augmentation_long_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/tree_augmentation_long_test
-exit_on_error
-echo ------------------------------------------- capacitated_facility_location_long_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/capacitated_facility_location_long_test --run_test=FacilityLocationLong
-exit_on_error
-echo ------------------------------------------- dreyfus_wagner_long_test    -------------------------------------------------------------------------------------
-./${build_dir}/test/dreyfus_wagner_long_test
-exit_on_error
-echo ------------------------------------------- facility_location_long_test    -------------------------------------------------------------------------------------
-./${build_dir}/test/facility_location_long_test
-exit_on_error
-echo ------------------------------------------- generalised_assignment_long_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/generalised_assignment_long_test
-exit_on_error
-echo ------------------------------------------- steiner_network_long_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/steiner_network_long_test
-exit_on_error
-echo -------------------------------------------zelikovsky_11_per_6_long_test     -------------------------------------------------------------------------------------
-./${build_dir}/test/zelikovsky_11_per_6_long_test
-exit_on_error
-echo ------------------------------------------- k_median_long_test          -------------------------------------------------------------------------------------
-./${build_dir}/test/k_median_long_test
-exit_on_error
-echo ------------------------------------------- scheduling_jobs_on_identical_parallel_machines_long_test      -------------------------------------------------------------------------------------
-./${build_dir}/test/scheduling_jobs_on_identical_parallel_machines_long_test
-exit_on_error
-echo ------------------------------------------- knapsack_long_test          -------------------------------------------------------------------------------------
-./${build_dir}/test/knapsack_long_test
-exit_on_error
-echo ------------------------------------------- knapsack_two_app_long_test  -------------------------------------------------------------------------------------
-./${build_dir}/test/knapsack_two_app_long_test
-exit_on_error
+
+for t in $tests; do
+    echo ------------------------------------------- $t 	 -------------------------------------------------------------------------------------
+    ./${build_dir}/test/$t $(option $t)
+    exit_on_error
+done
 
