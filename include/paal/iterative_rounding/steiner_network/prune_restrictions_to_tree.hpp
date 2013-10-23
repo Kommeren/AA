c3d419ea48b521a1a6cd49feabdf89fd50416f8e
Piotr Wygocki <wygocki@siekiera.mimuw.edu.pl>
2013-10-23 20:00:08 +0200

    function_output_itearator used in zelikowsky_11_6
    Change-Id: Id5005a13feb8a33ed190099073c7853d9aaee1f8

--- include/paal/iterative_rounding/steiner_network/prune_restrictions_to_tree.hpp
+++ include/paal/iterative_rounding/steiner_network/prune_restrictions_to_tree.hpp
@@ -9,67 +9,52 @@
 #define PRUNE_RESTRICTIONS_TO_TREE_HPP 
 
 #include <boost/range/irange.hpp>
+#include <boost/function_output_iterator.hpp>
 
 namespace paal {
 
 typedef std::vector<std::pair<unsigned, unsigned>> RestrictionsVector;
 
 template <typename G>
-struct EdgeToVertexPairsIterator { 
-    typedef EdgeToVertexPairsIterator value_type; 
-    typedef typename boost::graph_traits<G>::edge_descriptor Edge;
-    typedef typename boost::graph_traits<G>::vertex_descriptor Vertex;
+    struct EdgeToVertexPairsIterator {
+        typedef typename boost::graph_traits<G>::edge_descriptor Edge;
 
-    EdgeToVertexPairsIterator(const G & g, RestrictionsVector & resVec) : 
-        m_restrictionsVec(&resVec), m_g(&g) {}
+        EdgeToVertexPairsIterator(const G & g, RestrictionsVector & resVec) :
+            m_restrictionsVec(&resVec), m_g(&g) {}
 
-    EdgeToVertexPairsIterator & operator=(const EdgeToVertexPairsIterator &) = default;
-    EdgeToVertexPairsIterator & operator=(EdgeToVertexPairsIterator &&) = default;
-    
-    EdgeToVertexPairsIterator(const EdgeToVertexPairsIterator &) = default;
-    EdgeToVertexPairsIterator(EdgeToVertexPairsIterator &&) = default;
 
-    EdgeToVertexPairsIterator & operator*() {
-        return *this;
-    }
+        void operator()(Edge e) {
+            m_restrictionsVec->push_back(std::make_pair(source(e, *m_g), target(e, *m_g)));
+            m_restrictionsVec->back();
+        }
 
-    EdgeToVertexPairsIterator & operator++() { 
-        return *this;
-    }
-    
-    EdgeToVertexPairsIterator & operator++(int) {
-        return *this;
-    }
-
-    std::pair<unsigned, unsigned> & operator=(Edge e) {
-        m_restrictionsVec->push_back(std::make_pair(source(e, *m_g), target(e, *m_g)));
-        return m_restrictionsVec->back();
-    }
-    
-    RestrictionsVector * m_restrictionsVec; 
-    const G * m_g;
-};
+        RestrictionsVector * m_restrictionsVec;
+        const G * m_g;
+    };
 
 
 template <typename Restrictions>
-RestrictionsVector pruneRestrictionsToTree(Restrictions res,  int N) {
-    typedef decltype(std::declval<Restrictions>()(0,0)) Dist;
-    typedef boost::property < boost::edge_weight_t, Dist> EdgeProp;
-    typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::undirectedS,
-                        boost::no_property,  EdgeProp> TGraph;
+    RestrictionsVector pruneRestrictionsToTree(Restrictions res,  int N) {
+        typedef decltype(std::declval<Restrictions>()(0,0)) Dist;
+        typedef boost::property < boost::edge_weight_t, Dist> EdgeProp;
+        typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::undirectedS,
+                boost::no_property,  EdgeProp> TGraph;
 
-    RestrictionsVector resVec;
-    TGraph g(N);
-    for(int i : boost::irange(0, N)) {
-        for(int j : boost::irange(i, N)) {
-            add_edge(i, j, 
-                    EdgeProp(-std::max(res(i, j), res(j, i))),  g);
+        RestrictionsVector resVec;
+        TGraph g(N);
+        for(int i : boost::irange(0, N)) {
+            for(int j : boost::irange(i, N)) {
+                add_edge(i, j, 
+                        EdgeProp(-std::max(res(i, j), res(j, i))),  g);
+            }
         }
-    }
         
-    kruskal_minimum_spanning_tree(g, EdgeToVertexPairsIterator<TGraph>(g, resVec));
-    return resVec;
-}
+        EdgeToVertexPairsIterator<TGraph> addEdge(g, resVec);
+
+        kruskal_minimum_spanning_tree(g, 
+                boost::make_function_output_iterator(addEdge));
+        return std::move(resVec);
+    }
 
 
 }
