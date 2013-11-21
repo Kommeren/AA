d5dea9282d04aa17176abb5c9fa7b3508c754776
Piotr Wygocki <wygos@mimuw.edu.pl>
2013-11-21 13:26:30 +0100

    all_small_tests.cpp added
    Change-Id: I74ee523467b0e84132492504914c4b2115b3fe57

---
+++ test/all_small_tests.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file all_small_tests.cpp
+ * @brief 
+ * @author Piotr Wygocki
+ * @version 1.0
+ * @date 2013-11-18
+ */
+
+#define BOOST_TEST_MODULE scheduling_jobs_on_identical_parallel_machines
+#include <boost/test/unit_test.hpp>
